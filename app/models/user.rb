373d4925b2d9f0e65ae6e7b53428c188d614302f
Mikhail <mixasun@gmail.com>
2021-01-29 08:23:32 +0300

    game.users.users_player & game.users.users_referee

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
 
   has_many :game_users
   has_many :games, through: :game_users
+
+  scope :users_player, -> {joins(:games).merge(GameUser.player)}
+  scope :users_referee, -> {joins(:games).merge(GameUser.referee)}
 end
