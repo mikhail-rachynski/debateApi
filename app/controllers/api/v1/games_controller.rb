72f7dae2b7ffe5e3088260717261be72a6970fd7
MIkhail Rachynski <mixasun@gmail.com>
2021-03-23 20:18:42 +0300

    refactoring

--- app/controllers/api/v1/games_controller.rb
+++ app/controllers/api/v1/games_controller.rb
@@ -13,7 +13,8 @@ class Api::V1::GamesController < ApplicationController
 
   def create
     current_user.games.create(items_params)
-    json_response("ok", :created)
+    @games = Game.all.order(:id)
+    render 'index.json.jbuilder'
   end
 
   def update
@@ -26,63 +27,19 @@ class Api::V1::GamesController < ApplicationController
   end
 
   def add_player
-    player_limit = 9
     @game = Game.find(params[:id])
-    find_duplicate_user = @game.users.find_by id: current_user.id
-    if find_duplicate_user.nil?
-      if @game.users.count < player_limit && @game.status == Game.statuses.key(0)
-        GameUser.create(game: @game, user: current_user)
-        players_left = player_limit - @game.users.count
-        start_game if @game.users.count == player_limit
-        @games = Game.all.order(:id)
-        render 'index.json.jbuilder'
-      else
-        @error = "Players limit reached"
-        render 'api/v1/errors/error.json.jbuilder'
-      end
-    else
-      @error = "User has already been added"
-      render 'api/v1/errors/error.json.jbuilder'
-    end
+    @games = @game.add_player(current_user.id)
+    render 'index.json.jbuilder'
   end
 
   def delete_player
     @game = Game.find(params[:id])
-    if @game.status == Game.statuses.key(0)
-        @game_user = GameUser.find_by game_id: params[:id], user_id: params[:user_id]
-      unless @game_user.nil?
-        @game_user.destroy
-        head :no_content
-      else
-        head :not_found
-      end
-    end
-  end
-
-  def set_users_roles
-    count = 0
-    GameUser.where(game_id: @game.id).find_each do |item|
-      item.update(role: GameUser.roles.key(count))
-      count += 1
-    end
+    @game.delete_player(current_user.id)
   end
 
   def status
     @game = Game.find(params[:id])
     json_response(@game.status)
-  end
-
-  def start_game
-    set_users_roles
-    set_round
-    @game.update(status: 1)
-  end
-
-  def set_round
-    Round.create(game: @game, round_type: Round.round_types.key(0))
-    Round.create(game: @game, round_type: Round.round_types.key(1))
-    Round.create(game: @game, round_type: Round.round_types.key(2))
-    Round.create(game: @game, round_type: Round.round_types.key(3))
   end
 
   def get_rounds
--- app/models/game.rb
+++ app/models/game.rb
@@ -6,4 +6,53 @@ class Game < ApplicationRecord
   belongs_to :user
 
   has_many :rounds, dependent: :destroy
+
+  def add_player(current_user)
+    player_limit = 9
+    find_duplicate_user = self.users.find_by id: current_user
+    if find_duplicate_user.nil?
+      if self.users.count < player_limit && self.status == Game.statuses.key(0)
+        GameUser.create(game: self, user_id: current_user)
+        start_game if self.users.count == player_limit
+        return @games = Game.all.order(:id)
+      else
+        @error = "Players limit reached"
+        render 'api/v1/errors/error.json.jbuilder'
+      end
+    else
+      @error = "User has already been added"
+      render 'api/v1/errors/error.json.jbuilder'
+    end
+  end
+
+  def delete_player(current_user)
+    if self.status == Game.statuses.key(0)
+      @game_user = GameUser.find_by game_id: self.id, user_id: current_user
+      unless @game_user.nil?
+        @game_user.destroy
+      end
+    end
+  end
+
+  def set_users_roles
+    count = 0
+    GameUser.where(game_id: self.id).find_each do |item|
+      item.update(role: GameUser.roles.key(count))
+      count += 1
+    end
+  end
+
+
+  def start_game
+    set_users_roles
+    set_round
+    self.update(status: 1)
+  end
+
+  def set_round
+    Round.create(game: self, round_type: Round.round_types.key(0))
+    Round.create(game: self, round_type: Round.round_types.key(1))
+    Round.create(game: self, round_type: Round.round_types.key(2))
+    Round.create(game: self, round_type: Round.round_types.key(3))
+  end
 end
